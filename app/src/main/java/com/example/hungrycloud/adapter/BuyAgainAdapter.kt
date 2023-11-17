5889843fda97867f85b861953d3c5082394187a0
fabspulkit <pulkitchausali.22@gmail.com>
2023-11-17 17:41:54 +0530

    History Fragment Basic Ui Completed

---
+++ app/src/main/java/com/example/hungrycloud/adapter/BuyAgainAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.hungrycloud.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hungrycloud.data.MenuItemData
+import com.example.hungrycloud.databinding.BuyAgainItemBinding
+
+class BuyAgainAdapter(val foodItemList: ArrayList<MenuItemData>) :
+    RecyclerView.Adapter<BuyAgainAdapter.BuyAgainViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuyAgainViewHolder {
+        val binding =
+            BuyAgainItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BuyAgainViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BuyAgainViewHolder, position: Int) {
+        holder.bind(foodItemList[position])
+    }
+
+    override fun getItemCount(): Int = foodItemList.size
+
+
+    class BuyAgainViewHolder(private val binding: BuyAgainItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(curMenuItemData: MenuItemData) {
+            binding.apply {
+                recentBuyFoodnameTextview.text = curMenuItemData.foodName
+                recentBuyFoodpriceTextview.text = curMenuItemData.foodPrice
+                recentBuyFoodimageTextview.setImageResource(curMenuItemData.foodImage)
+            }
+        }
+
+    }
+}
